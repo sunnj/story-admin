220eed9d7a5f5e4610900ca783d6462e49e61fd8
nj.sun <sunnj87@hotmail.com>
2019-08-18 21:14:10 +0800

    增加mybatis-plus配置使分页生效

---
+++ src/main/java/com/story/storyadmin/config/MybatisPlusConfig.java
@@ -0,0 +1,16 @@
+package com.story.storyadmin.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor page = new PaginationInterceptor();
+        page.setDialectType("mysql");
+        return page;
+    }
+}
