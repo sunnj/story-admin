104ffa3d067f689de584f96eb7e161b9bc8f76ba
nj.sun <sunnj87@hotmail.com>
2019-10-22 17:07:11 +0800

    优化代码swagger配置

--- src/main/java/com/story/storyadmin/config/SwaggerCofing.java
+++ src/main/java/com/story/storyadmin/config/SwaggerCofing.java
@@ -2,6 +2,8 @@ package com.story.storyadmin.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -13,7 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @Configuration
-public class SwaggerCofing {
+public class SwaggerCofing extends WebMvcConfigurationSupport {
     @Bean
     public Docket apiConfig(){
         return new Docket(DocumentationType.SWAGGER_2)
@@ -35,4 +37,14 @@ public class SwaggerCofing {
                 .build();
     }
 
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 解决静态资源无法访问
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        // 解决swagger无法访问
+        registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        // 解决swagger的js文件无法访问
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+    }
 }
