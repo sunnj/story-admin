e1f265d4e3741caae10e16a2cb7da748f3f3ad75
nj.sun <sunnj87@hotmail.com>
2019-12-27 11:11:25 +0800

    优化druid配置

--- src/main/java/com/story/storyadmin/StoryAdminApplication.java
+++ src/main/java/com/story/storyadmin/StoryAdminApplication.java
@@ -1,5 +1,6 @@
 package com.story.storyadmin;
 
+import com.alibaba.druid.support.http.StatViewServlet;
 import com.story.storyadmin.config.props.DbBackupProperties;
 import com.story.storyadmin.config.shiro.security.JwtProperties;
 import org.mybatis.spring.annotation.MapperScan;
@@ -7,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 //@ImportResource(locations={"classpath:spring/spring-config-*.xml"})
@@ -22,6 +25,18 @@ public class StoryAdminApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(StoryAdminApplication.class, args);
+    }
+
+
+    @Bean
+    public ServletRegistrationBean<StatViewServlet> druidStatViewServlet() {
+        ServletRegistrationBean<StatViewServlet> registrationBean = new ServletRegistrationBean<>(new StatViewServlet(),  "/druid/*");
+        registrationBean.addInitParameter("allow", "127.0.0.1");// IP白名单 (没有配置或者为空，则允许所有访问)
+        registrationBean.addInitParameter("deny", "");// IP黑名单 (存在共同时，deny优先于allow)
+        registrationBean.addInitParameter("loginUsername", "root");
+        registrationBean.addInitParameter("loginPassword", "root@1234qwer");
+        registrationBean.addInitParameter("resetEnable", "false");
+        return registrationBean;
     }
 
 }
